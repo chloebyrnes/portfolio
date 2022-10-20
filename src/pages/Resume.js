f27b3b7ba64c1163d32600db01bdcd979c56f1be
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-10-20 16:45:41 -0400

    new connection for deploy

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,25 +1,92 @@
 import * as React from "react";
 import HeaderBar from "../components/Header/HeaderBar.js";
 import Footer from "../components/Footer/Footer.js";
+import StoryKeeper from "../images/storykeeper.png";
+import RunBuddy from "../images/RunBuddy.png";
+import NowWereCookin from "../images/NowWereCookin.png";
+import Horiseon from "../images/Horiseon.png";
 
-const Resume = () => {
+const Portfolio = () => {
   return (
-    <main class="bg-stone-200">
+    <div class="bg-stone-200">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+      </style>
+
       <HeaderBar />
 
-      <div>
-        <h1>hi</h1>
+      <div class="py-10 bg-white">
+        <div class="max-w-xl mx-auto lg:max-w-7xl lg:px-8">
+          <div class="container mx-auto flex justify-center bg-white">
+            <h1 class="text-4xl text-center font-semibold text-stone-800">
+              Portfolio
+            </h1>
+          </div>
+          <dl class="mt-20 lg:space-y-0 lg:grid lg:grid-cols-3 grid grid-cols-2 lg:gap-8 gap-2 md:gap-0 space-y-0">
+            <div class="border  md:p-10 p-5 rounded-3xl">
+              <dt class="">
+                <img src={StoryKeeper}></img>
+
+                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
+                  The Story Keeper
+                </p>
+              </dt>
+            </div>
+            <div class="border md:p-10 p-5 rounded-3xl">
+              <dt class="">
+                <img src={RunBuddy}></img>
+
+                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
+                  Run Buddy
+                </p>
+              </dt>
+            </div>
+            <div class="border  md:p-10 p-5 rounded-3xl">
+              <dt class="">
+                <img src={Horiseon}></img>
+
+                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
+                  Horiseon
+                </p>
+              </dt>
+            </div>
+            <div class="border  md:p-10 p-5 rounded-3xl">
+              <dt class="">
+                <img src={NowWereCookin}></img>
+
+                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
+                  Now We're Cookin
+                </p>
+              </dt>
+            </div>
+            <div class="border  md:p-10 p-5 rounded-3xl">
+              <dt class="">
+                <img src={StoryKeeper}></img>
+
+                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
+                  The Story Keeper
+                </p>
+              </dt>
+            </div>
+            <div class="border  md:p-10 p-5 rounded-3xl">
+              <dt class="">
+                <img src={StoryKeeper}></img>
+
+                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
+                  The Story Keeper
+                </p>
+              </dt>
+            </div>
+          </dl>
+        </div>
       </div>
 
-      <div
-        class="fixed
-             inset-x-0
-             bottom-0"
-      >
+      <div>
         <Footer />
       </div>
-    </main>
+    </div>
   );
 };
 
-export default Resume;
+export default Portfolio;

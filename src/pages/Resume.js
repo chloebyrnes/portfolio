02e0090a6062425369fa8799d292f1180a375fa3
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-10-20 16:59:48 -0400

    test

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -20,62 +20,13 @@ const Resume = () => {
         <div class="max-w-xl mx-auto lg:max-w-7xl lg:px-8">
           <div class="container mx-auto flex justify-center bg-white">
             <h1 class="text-4xl text-center font-semibold text-stone-800">
-              Portfolio
+              Resume
             </h1>
           </div>
           <dl class="mt-20 lg:space-y-0 lg:grid lg:grid-cols-3 grid grid-cols-2 lg:gap-8 gap-2 md:gap-0 space-y-0">
             <div class="border  md:p-10 p-5 rounded-3xl">
               <dt class="">
-                <img src={StoryKeeper}></img>
-
-                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
-                  The Story Keeper
-                </p>
-              </dt>
-            </div>
-            <div class="border md:p-10 p-5 rounded-3xl">
-              <dt class="">
-                <img src={RunBuddy}></img>
-
-                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
-                  Run Buddy
-                </p>
-              </dt>
-            </div>
-            <div class="border  md:p-10 p-5 rounded-3xl">
-              <dt class="">
-                <img src={Horiseon}></img>
-
-                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
-                  Horiseon
-                </p>
-              </dt>
-            </div>
-            <div class="border  md:p-10 p-5 rounded-3xl">
-              <dt class="">
-                <img src={NowWereCookin}></img>
-
-                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
-                  Now We're Cookin
-                </p>
-              </dt>
-            </div>
-            <div class="border  md:p-10 p-5 rounded-3xl">
-              <dt class="">
-                <img src={StoryKeeper}></img>
-
-                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
-                  The Story Keeper
-                </p>
-              </dt>
-            </div>
-            <div class="border  md:p-10 p-5 rounded-3xl">
-              <dt class="">
-                <img src={StoryKeeper}></img>
-
-                <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-stone-800">
-                  The Story Keeper
-                </p>
+                <p>Test</p>
               </dt>
             </div>
           </dl>

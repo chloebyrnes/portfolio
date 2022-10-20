f926eac25964309fc908ddaffe42bd9a6ad28986
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-10-19 21:58:11 -0400

    test

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,6 +10,51 @@ const About = () => {
           </h1>
         </div>
       </div>
+
+      <dl class="mt-20 lg:space-y-0 lg:grid lg:grid-cols-3 grid grid-cols-2 lg:gap-8 gap-2 md:gap-0 space-y-0">
+        <div class="border  md:p-10 p-5 rounded-3xl">
+          <dt class="">
+            <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+              The Story Keeper
+            </p>
+          </dt>
+        </div>
+        <div class="border md:p-10 p-5 rounded-3xl">
+          <dt class="">
+            <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+              Run Buddy
+            </p>
+          </dt>
+        </div>
+        <div class="border  md:p-10 p-5 rounded-3xl">
+          <dt class="">
+            <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+              Horiseon
+            </p>
+          </dt>
+        </div>
+        <div class="border  md:p-10 p-5 rounded-3xl">
+          <dt class="">
+            <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+              Now We're Cookin
+            </p>
+          </dt>
+        </div>
+        <div class="border  md:p-10 p-5 rounded-3xl">
+          <dt class="">
+            <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+              The Story Keeper
+            </p>
+          </dt>
+        </div>
+        <div class="border  md:p-10 p-5 rounded-3xl">
+          <dt class="">
+            <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+              The Story Keeper
+            </p>
+          </dt>
+        </div>
+      </dl>
     </div>
   );
 };

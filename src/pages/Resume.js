667f6ffb2e87f15eb0b9c43911cc87919a914de5
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-10-19 22:00:29 -0400

    another

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -35,10 +35,55 @@ const Resume = () => {
         <div class="max-w-xl mx-auto lg:max-w-7xl lg:px-8">
           <div class="container mx-auto flex justify-center bg-white">
             <h1 class="text-4xl text-center font-semibold text-sky-900">
-              Resume
+              About Me
             </h1>
           </div>
         </div>
+
+        <dl class="mt-20 lg:space-y-0 lg:grid lg:grid-cols-3 grid grid-cols-2 lg:gap-8 gap-2 md:gap-0 space-y-0">
+          <div class="border  md:p-10 p-5 rounded-3xl">
+            <dt class="">
+              <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+                The Story Keeper
+              </p>
+            </dt>
+          </div>
+          <div class="border md:p-10 p-5 rounded-3xl">
+            <dt class="">
+              <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+                Run Buddy
+              </p>
+            </dt>
+          </div>
+          <div class="border  md:p-10 p-5 rounded-3xl">
+            <dt class="">
+              <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+                Horiseon
+              </p>
+            </dt>
+          </div>
+          <div class="border  md:p-10 p-5 rounded-3xl">
+            <dt class="">
+              <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+                Now We're Cookin
+              </p>
+            </dt>
+          </div>
+          <div class="border  md:p-10 p-5 rounded-3xl">
+            <dt class="">
+              <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+                The Story Keeper
+              </p>
+            </dt>
+          </div>
+          <div class="border  md:p-10 p-5 rounded-3xl">
+            <dt class="">
+              <p class="text-center mt-8 md:text-2xl text-lg leading-6 font-bold text-sky-900">
+                The Story Keeper
+              </p>
+            </dt>
+          </div>
+        </dl>
       </div>
 
       <div
